f8daf8c537b486f603690eaf233dbedf98dd3227
Emmy Nguyen <145530039+tastytough@users.noreply.github.com>
2025-12-08 20:00:59 -0800

    Add accordion style for dropdown menu on mobile

--- js/navbar.js
+++ js/navbar.js
@@ -3,6 +3,7 @@
    =================================== */
 
 document.addEventListener("DOMContentLoaded", function () {
+  // Mobile Menu Toggle
   const navToggle = document.querySelector(".nav-toggle");
   const navMenu = document.querySelector(".nav-menu");
   const navLinks = document.querySelectorAll(".nav-link");
@@ -50,6 +51,52 @@ document.addEventListener("DOMContentLoaded", function () {
       navMenu.classList.remove("active");
       navToggle.setAttribute("aria-expanded", "false");
       body.style.overflow = "";
+    }
+  });
+
+  // Dropdown Menu
+  const dropdownItems = document.querySelectorAll(".dropdown");
+
+  dropdownItems.forEach((dropdown) => {
+    const dropdownLink = dropdown.querySelector(".nav-link");
+    const dropdownMenu = dropdown.querySelector(".dropdown-menu");
+
+    if (dropdownLink && dropdownMenu) {
+      dropdownLink.addEventListener("click", function (e) {
+        if (window.innerWidth <= 1024) {
+          e.preventDefault();
+          dropdown.classList.toggle("active");
+          const isExpanded = dropdown.classList.contains("active");
+          dropdownLink.setAttribute("aria-expanded", isExpanded);
+
+          dropdownItems.forEach((otherDropdown) => {
+            if (
+              (otherDropdown !== dropdown) &
+              otherDropdown.classList.contains("active")
+            ) {
+              otherDropdown.classList.remove("active");
+              otherDropdown
+                .querySelector(".nav-link")
+                .setAttribute("aria-expanded", "false");
+            }
+          });
+        }
+      });
+
+      const dropdownLinks = dropdownMenu.querySelectorAll(".dropdown-link");
+      dropdownLinks.forEach((link) => {
+        link.addEventListener("click", function () {
+          if (windown.innerWidth <= 1024) {
+            dropdown.classList.remove("active");
+            dropdownLink.setAttribute("aria-expanded", "false");
+
+            navToggle.classList.remove("active");
+            navMenu.classList.remove("active");
+            navToggle.setAttribute("aria-expanded", "false");
+            body.style.overflow = "";
+          }
+        });
+      });
     }
   });
 });
